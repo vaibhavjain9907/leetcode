b7f3b6e16ed24aba1da45a3258f7ad1024f0a157
vaibhav jain <vaibhavjain9907@gmail.com>
2026-08-23 23:11:57 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (74.44%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,23 +1,65 @@
 class Solution {
+private:
+    void swapIfGreater(vector<int>& nums1, vector<int>& nums2,
+                       int idx1, int idx2) {
+        if(nums1[idx1] > nums2[idx2]) {
+            swap(nums1[idx1], nums2[idx2]);
+        }
+    }
+
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int left = m-1; 
-        int right = 0;
-       
-        while(left >= 0 && right < n){
-            if(nums1[left] > nums2[right]){
-                swap(nums1[left],nums2[right]);
-                left--;
+
+        int len = m + n;
+        int gap = (len / 2) + (len % 2);
+
+        while(gap > 0) {
+
+            int left = 0;
+            int right = left + gap;
+
+            while(right < len) {
+
+                // nums1 & nums2
+                if(left < m && right >= m) {
+                    swapIfGreater(
+                        nums1, nums2,
+                        left,
+                        right - m
+                    );
+                }
+
+                // nums2 & nums2
+                else if(left >= m) {
+                    swapIfGreater(
+                        nums2, nums2,
+                        left - m,
+                        right - m
+                    );
+                }
+
+                // nums1 & nums1
+                else {
+                    swapIfGreater(
+                        nums1, nums1,
+                        left,
+                        right
+                    );
+                }
+
+                left++;
                 right++;
             }
-            else{
+
+            if(gap == 1)
                 break;
-            }
+
+            gap = (gap / 2) + (gap % 2);
         }
-        sort(nums1.begin(),nums1.begin()+m);
-        sort(nums2.begin(),nums2.begin()+n);
+
+        
         for(int i = 0; i < n; i++) {
-    nums1[m + i] = nums2[i];
-}
+            nums1[m + i] = nums2[i];
+        }
     }
 };
